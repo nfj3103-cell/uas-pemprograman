806ada8f4cb74ad2d55a61d4afd7975d520ba5bc
lydaramadhani <lydaramadhani05@gmail.com>
2026-07-23 23:52:22 +0700

    Menambahkan model booking untuk transaksi tiket

--- events/models.py
+++ events/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Event(models.Model):
@@ -95,3 +96,49 @@ class Ticket(models.Model):
     def __str__(self):
 
         return self.nama_tiket
+class Booking(models.Model):
+
+    STATUS_CHOICES = (
+
+        ('PENDING', 'Menunggu Pembayaran'),
+        ('PAID', 'Sudah Dibayar'),
+        ('CANCEL', 'Dibatalkan'),
+
+    )
+
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+
+
+    ticket = models.ForeignKey(
+        Ticket,
+        on_delete=models.CASCADE
+    )
+
+
+    jumlah = models.IntegerField(
+        default=1
+    )
+
+
+    total_harga = models.IntegerField()
+
+
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default='PENDING'
+    )
+
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+
+    def __str__(self):
+
+        return f"{self.user.username} - {self.ticket.nama_tiket}"
