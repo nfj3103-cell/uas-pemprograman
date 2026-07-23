3bff9fcb87834405626aa5fe91a8b58656d33441
lydaramadhani <lydaramadhani05@gmail.com>
2026-07-23 23:43:37 +0700

    Membuat model ticket untuk sistem event

--- events/models.py
+++ events/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Event(models.Model):
 
+
     STATUS_CHOICES = (
 
         ('AKTIF','Aktif'),
@@ -49,3 +50,48 @@ class Event(models.Model):
     def __str__(self):
 
         return self.nama_event
+class Ticket(models.Model):
+
+    CATEGORY_CHOICES = (
+
+        ('REGULER', 'Reguler'),
+        ('VIP', 'VIP'),
+
+    )
+
+
+    event = models.ForeignKey(
+        Event,
+        on_delete=models.CASCADE,
+        related_name='tickets'
+    )
+
+
+    nama_tiket = models.CharField(
+        max_length=100
+    )
+
+
+    kategori = models.CharField(
+        max_length=20,
+        choices=CATEGORY_CHOICES,
+        default='REGULER'
+    )
+
+
+    harga = models.IntegerField()
+
+
+    stok = models.IntegerField(
+        default=0
+    )
+
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+
+    def __str__(self):
+
+        return self.nama_tiket
