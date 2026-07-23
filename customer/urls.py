2381b0b277e2ef24a1e0f7c25966b6a71b7b36a3
lydaramadhani <lydaramadhani05@gmail.com>
2026-07-23 15:32:21 +0700

    perbaikan masuk

--- customer/urls.py
+++ customer/urls.py
@@ -5,6 +5,13 @@ from . import views
 urlpatterns = [
 
     path(
+        "",
+        views.home,
+        name="home"
+    ),
+
+
+    path(
         "dashboard-customer/",
         views.dashboard_customer,
         name="dashboard_customer"
--- customer/views.py
+++ customer/views.py
@@ -1,4 +1,19 @@
 from django.shortcuts import render
+from django.shortcuts import render
+
+def dashboard_customer(request):
+
+    return render(
+        request,
+        "customer/dashboard.html"
+        
+    )
+def home(request):
+
+    return render(
+        request,
+        "customer/home.html"
+    )
 
 
 def dashboard_customer(request):
